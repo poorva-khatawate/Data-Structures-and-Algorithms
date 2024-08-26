08cc933069f88ac74587be2cb7b8c04b1183a249
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-26 23:32:28 +0530

    oddeven

---
+++ Oddeven.cpp
@@ -0,0 +1,14 @@
+//check if a number is odd or even.
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Even";
+    }
+    else{
+        cout<<"Odd";
+    }
+}
