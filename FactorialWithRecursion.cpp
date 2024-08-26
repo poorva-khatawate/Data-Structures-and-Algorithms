24a95f68ffd7843723e9089f7176188b462a71b5
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-27 00:16:39 +0530

    factorialr

---
+++ FactorialWithRecursion.cpp
@@ -0,0 +1,16 @@
+//Factorial of a number using recursion
+#include <iostream>
+using namespace std;
+
+int fact(int n){
+    if(n==0){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
