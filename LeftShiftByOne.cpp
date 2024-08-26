03e9ebd3580b6454219f614971887f61a8146fdb
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-26 23:56:24 +0530

    leftshift

---
+++ LeftShiftByOne.cpp
@@ -0,0 +1,24 @@
+//Left shift an array by one
+//Input: 1 2 3 4 5
+//Output: 2 3 4 5 1
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int temp=arr[0];
+    for(int i=1;i<n;i++){
+        arr[i-1]=arr[i];
+    }
+    arr[n-1]=temp;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
