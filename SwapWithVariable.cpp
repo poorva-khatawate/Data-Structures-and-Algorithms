0e6379ed58d166082c2cb31c2fa81c96653e5b1a
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-26 23:23:42 +0530

    SwapWithoutVariable

---
+++ SwapWithVariable.cpp
@@ -0,0 +1,13 @@
+//Swap numbers without variables
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<a<<" "<<b;
+    return 0;
+}
