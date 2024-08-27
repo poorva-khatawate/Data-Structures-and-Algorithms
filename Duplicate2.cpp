5ab33071b8066f48674ce65402c188ca9936a2bb
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-28 00:29:04 +0530

    duplicate2

---
+++ Duplicate2.cpp
@@ -0,0 +1,24 @@
+//find duplicate elements in an array
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int freq[n]={0};
+    for(int i=0;i<n;i++){
+        if(freq[a[i]]!=0){
+            cout<<a[i]<<" ";
+            break;
+        }
+        else{
+            freq[a[i]]++;
+        }
+    }
+    return 0;
+}
