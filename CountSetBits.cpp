a8cd7bc9b9049035808e80bc8f57392d6659a60f
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-27 23:58:38 +0530

    cntsetbits

---
+++ CountSetBits.cpp
@@ -0,0 +1,22 @@
+//Count the number of set bits in a number
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int cnt=0;
+    while(n!=0){
+        if(n%2==1){
+            cnt++;
+            
+        }
+        n=n/2;
+    }
+    cout<<cnt;
+    return 0;
+}
+// Time Complexity: O(log(n))
+// Space Complexity: O(1)
+// Input: 5
+// Output: 2
