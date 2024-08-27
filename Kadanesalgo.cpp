5f56170d686c4a20430596396508a0a2f76a8f87
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-27 10:37:36 +0530

    kadane

---
+++ Kadanesalgo.cpp
@@ -0,0 +1,21 @@
+//Max sum of a subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int maxi=INT_MIN;
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum+=a[j];
+            maxi=max(maxi,sum);
+        }
+    }
+    cout<<maxi<<endl;
+}
