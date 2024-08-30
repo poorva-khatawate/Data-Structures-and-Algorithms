a8b95f52a669db2b9780bfbe394d4ca9bf3ab891
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-30 23:40:30 +0530

    pattern

---
+++ Patterns/n rows each row ele increases by 1.cpp
@@ -0,0 +1,20 @@
+//input 3
+//output
+/*
+1
+1 2
+1 2 3
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ Patterns/same ele row.cpp
@@ -0,0 +1,20 @@
+//input 3
+//output
+/*
+1 
+2 2
+3 3 3
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
