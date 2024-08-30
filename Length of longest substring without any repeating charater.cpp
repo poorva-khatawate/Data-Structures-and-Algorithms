d1e72ffc34e149f8b8f7339d2f1a89d0295dae84
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-30 23:06:07 +0530

    longest substring without repeating character

--- Length of longest substring without any repeating charater.cpp
+++ Length of longest substring without any repeating charater.cpp
@@ -1,27 +1,27 @@
-//Length of longest substring without any repeating character
 #include <iostream>
-#include <vector>
-#include <climits>
 #include <string>
 #include <set>
 using namespace std;
-int main(){
+
+int main() {
     string s;
-    cin>>s;
-    int n=s.length();
-    
-    int maxlen=INT_MIN;
-    for(int i=0;i<n;i++){
+    cin >> s;
+    int n = s.length();
+
+    int maxlen = 0;  // Start maxlen with 0 instead of INT_MIN
+    for (int i = 0; i < n; i++) {
         set<char> st;
-        for(int j=i;j<n;j++){
-            if(st.find(s[j])!=st.end()){
-                 maxlen=max(maxlen,j-i);
-                    break;
+        for (int j = i; j < n; j++) {
+            if (st.find(s[j]) != st.end()) {
+                maxlen = max(maxlen, j - i);  // Update maxlen if repeat found
+                break;
             }
             st.insert(s[j]);
-           
         }
+        // Update maxlen in case the loop completes without finding a repeat
+        maxlen = max(maxlen, static_cast<int>(st.size()));
     }
-    cout<<maxlen;
+
+    cout << maxlen << endl;
     return 0;
 }
