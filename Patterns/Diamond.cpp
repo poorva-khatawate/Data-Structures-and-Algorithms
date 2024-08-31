34ef56d5818e5e5dab4859e1675fb22f01963c20
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-31 23:15:49 +0530

    diamond

---
+++ Patterns/Diamond.cpp
@@ -0,0 +1,47 @@
+//print diamond pattern
+//input n 4
+//output
+/*
+    *
+   * *
+  * * *
+ * * * *
+ * * * *
+  * * *
+   * *
+    *
+*/
+#include <iostream>
+using namespace std;
+void upper(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+void lower(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<n-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+    upper(n);
+    lower(n);
+    return 0;
+}
