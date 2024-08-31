aff8bd8a542a2fcfeedb9f5475205e3255a8f56e
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-08-31 23:51:14 +0530

    pattern

---
+++ Patterns/A B altering rows.cpp
@@ -0,0 +1,26 @@
+//input n 4
+//output
+/*
+   A
+  ABA
+ ABCBA
+ABCDCBA
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<=i;j++){
+            cout<<char('A'+j);
+        }
+        for(int j=i-1;j>=0;j--){
+            cout<<char('A'+j);
+        }
+        cout<<endl;
+    }
+}
