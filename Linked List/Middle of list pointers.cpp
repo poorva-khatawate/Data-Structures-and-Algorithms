d6d1fb84f23c844db949bd6625a34a86338aef5b
poorva-khatawate <01fe21bcs302@kletech.ac.in>
2024-09-05 20:15:48 +0530

    linkedlist

---
+++ Linked List/Middle of list pointers.cpp
@@ -0,0 +1,37 @@
+//find middle of list
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+};
+
+Node* findMiddle(Node* head){
+    Node* slow=head;
+    Node* fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+
+
+int main(){
+    Node *head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(30);
+    head->next->next->next=new Node(40);
+    head->next->next->next->next=new Node(50);
+    head->next->next->next->next->next=new Node(60);
+    head->next->next->next->next->next->next=new Node(70);
+
+    head=findMiddle(head);
+    cout<<head->data;
+    return 0;
+}
